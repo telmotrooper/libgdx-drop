57a521c181eb7d969210738bcdb2a174a3508b6c
Telmo <telmo.trooper@gmail.com>
2016-07-24 19:33:36 -0300

    Added icon for desktop

--- desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.badlogic.drop.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.drop.Drop;
@@ -8,6 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Drop";
+		config.addIcon("images/droplet_icon.png", Files.FileType.Internal);
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new Drop(), config);
