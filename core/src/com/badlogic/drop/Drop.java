8160ff4d18e1972f1936095bd925f9ec57fc0b0b
Telmo <telmo.trooper@gmail.com>
2016-07-24 20:35:28 -0300

    Added freetype extension and removed HTML support

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -1,8 +1,11 @@
 package com.badlogic.drop;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class Drop extends Game {
 	public SpriteBatch batch;
@@ -10,7 +13,12 @@ public class Drop extends Game {
 
 	public void create() {
 		batch = new SpriteBatch();
-		font = new BitmapFont(); // default LibGDX Arial font
+
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Jellee-Roman.ttf"));
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+		parameter.size = 20;
+
+		font = generator.generateFont(parameter);
 		this.setScreen(new MainMenuScreen(this));
 	}
 
